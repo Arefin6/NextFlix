93faff0d24e05bae020b7135043a43cfa37c3aa2
arefin6 <arefinhossain3@gmail.com>
2022-06-16 16:14:53 +0600

    Banner Section done

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Banner from './Components/Banner'
 
 export default function Home() {
   return (
@@ -10,6 +11,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Netflix</h1>
+      <Banner
+       title="Clifford the red dog"
+       subTitle="a very cute dog"
+       imgUrl="/static/clifford.jpg"
+      />
     </div>
   )
 }
