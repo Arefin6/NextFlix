88caac4979a1d8dd8f438162b8be430b6f1f8e7d
arefin6 <arefinhossain3@gmail.com>
2022-07-06 13:33:40 +0600

    popular videos completed

--- libs/videos.js
+++ libs/videos.js
@@ -1,20 +1,46 @@
-import videoTestData from "../data/videos.json";
 
-export const getVideos = async()=>{
+export const getCommonVideos = async(URL)=>{
 
-    const apikey = process.env.Youtube_API_Key;
+    try {
+        
+        const Youtube_API_Key = process.env.Youtube_API_Key;
+
+        const BASE_URL = "youtube.googleapis.com/youtube/v3";
     
-    const url=`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=disney%20trailer&key=${apikey}`;
+        const url=`https://${BASE_URL}/${URL}&maxResults=25&key=${Youtube_API_Key}`;
+    
+        const response = await fetch(url);
+    
+        const data = await response.json();
 
-    const response = await fetch(url);
+         if(data?.error){
+            console.error("Youtube API error", data.error);
+            return [];
+         }
+        
+        return data.items.map(item =>{
+            const id = item?.id?.videoId || item.id;
+            return{
+                title: item.snippet.title,
+                imgUrl:item.snippet.thumbnails.high.url,
+                id
+            }
+        });
 
-    const data = await response.json()
-
-    return data.items.map(item =>{
-        return{
-            title: item.snippet.title,
-            imgUrl:item.snippet.thumbnails.high.url,
-            id:item?.id?.videoId
-        }
-    });
+    } 
+    catch (error) {     
+        console.error("Something went wrong with video library", error);
+        return [];
+    }
+   
 }
+
+export const getVideos = (searchQuery) => {
+    const URL = `search?part=snippet&q=${searchQuery}&type=video`;
+    return getCommonVideos(URL);
+ };
+
+ export const getPopularVideos = () => {
+    const URL = `videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=BD`;
+    return getCommonVideos(URL);
+ };
