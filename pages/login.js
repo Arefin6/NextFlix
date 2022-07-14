3c2baa4f23b1425b12a3f0dbead0de0cb872785b
arefin6 <arefinhossain3@gmail.com>
2022-07-14 13:23:57 +0600

    login degine done

--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,37 @@
 import Head from "next/head";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Login.module.css";
 
 const Login = () => {
+   
+  const [email,setEmail] = useState("")
+  const [msg,setMsg] = useState("")
+
+  const handleEmailChange = (e) =>{
+    setMsg("")
+    setEmail(e.target.value)
+  }
+
+  const handleLoginWithEmail = (e) => {
+    e.preventDefault();
+    if(email){
+      // do 
+    }
+    else{
+      setMsg("Enter a valid Email Address")
+    }
+  };
+
+  
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>NetFlix SignIn</title>
       </Head>
 
-      <header>
+      <header className={styles.header}>
         <div className={styles.headerWrapper}>
           <Link className={styles.logoLink} href="/">
             <div className={styles.logoWrapper}>
@@ -25,6 +45,21 @@ const Login = () => {
           </Link>
         </div>
       </header>
+      <main className={styles.main}>
+        <div className={styles.mainWrapper}>
+          <h1 className={styles.signInHeader}>Sign In</h1>
+          <input
+            type="email"
+            placeholder="Email Address"
+            className={styles.emailInput}
+            onChange = {(e) =>handleEmailChange(e)}
+          />
+          <p className={styles.userMsg}>{msg}</p>
+          <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
+            Sign In
+          </button>
+        </div>
+      </main>
     </div>
   );
 };
