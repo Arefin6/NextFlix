5b6d3d0afe6455a8741f97a6d430bbf274922884
arefin6 <arefinhossain3@gmail.com>
2022-07-21 12:54:34 +0600

    Started with modal

--- pages/video/[videoid].js
+++ pages/video/[videoid].js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Modal from 'react-modal';
+import styles from '../../styles/video.module.css';
+
+
+Modal.setAppElement("#__next")
 
 const Video = () => {
     const router = useRouter()
-    console.log(router)
     return (
-        <div>
-           <h2>Video Page</h2> 
+        <div className={styles.container}>
+           <Modal isOpen={true} contentLabel="Watch The video" className={styles.modal}  onRequestClose={() =>router.back()} overlayClassName={styles.overlay}>
+              <div>Modal Body</div>
+           </Modal>
         </div>
     );
 };
