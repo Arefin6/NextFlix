705d2a35e24f45ad664fc23fcc6013f017e553e8
arefin6 <arefinhossain3@gmail.com>
2022-08-01 12:57:56 +0600

    Some Problem with graphql

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,15 +1,29 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 import {magicAdmin} from '../../libs/magic';
+import jwt from "jsonwebtoken";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async function (req,res){
    if(req.method === "POST"){
    try {
      const auth = req.headers.authorization;
      const didToken = auth ? auth.substr(7) : "";
-     console.log({didToken})
 
      const metaData = await magicAdmin.users.getMetadataByToken(didToken)
-     console.log({metaData})
+     const token = jwt.sign(
+      {
+        ...metaData,
+        iat: Math.floor(Date.now() / 1000),
+        exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
+        "https://hasura.io/jwt/claims": {
+          "x-hasura-allowed-roles": ["user", "admin"],
+          "x-hasura-default-role": "user",
+          "x-hasura-user-id": `${metaData.issuer}`,
+        },
+      },
+      process.env.JWT_SECRET
+    );
+
+    console.log({token})
 
      res.send({done:true})
    } catch (error) {
