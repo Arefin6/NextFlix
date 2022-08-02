6c4470b2a78d5c73adb5ab6e04353aab2b200732
arefin6 <arefinhossain3@gmail.com>
2022-08-02 14:19:34 +0600

    Hasura Issue Solved

--- libs/db/hasura.js
+++ libs/db/hasura.js
@@ -1,26 +1,22 @@
-/*
-This is an example snippet - you should consider tailoring it
-to your service.
-*/
+async function queryHasuraGQL(operationsDoc, operationName, variables, token) {
+  const result = await fetch(process.env.NEXT_PUBLIC_HASURA_ADMIN_URl, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      query: operationsDoc,
+      variables: variables,
+      operationName: operationName,
+    }),
+  });
 
-async function fetchGraphQL(operationsDoc, operationName, variables) {
-    const result = await fetch(process.env.NEXT_PUBLIC_HASURA_ADMIN_URl,
-      {
-        method: "POST",
-        headers:{
-            "x-hasura-admin-secret": process.env.NEXT_PUBLIC_HASURA_ADMIN_SCRECT
-        },
-        body: JSON.stringify({
-          query: operationsDoc,
-          variables: variables,
-          operationName: operationName
-        })
-      }
-    );
-  
-    return await result.json();
-  }
-  
+  return await result.json();
+}
+
+
+function fetchMyQuery() {
   const operationsDoc = `
     query MyQuery {
       users {
@@ -30,37 +26,18 @@ async function fetchGraphQL(operationsDoc, operationName, variables) {
         publicAddress
       }
     }
-    
-    mutation MyMutation {
-      insert_users_one(object: {email: "arefin@gmail.com", id: 10, issuer: "arefin", publicAddress: "did:pkj"}) {
-        id
-      }
-    }
-    
-    query MyQuery{
-      __typename
-    }
   `;
-  
-  
-  function fetchMyQuery() {
-    return fetchGraphQL(
-      operationsDoc,
-      "MyQuery",
-      {}
-    );
-  }
+  return queryHasuraGQL(operationsDoc, "MyQuery", {});
+}
 
 
- export async function startFetchMyQuery() {
-    const { errors, data } = await fetchMyQuery();
-  
-    if (errors) {
-      // handle those errors like a pro
-      console.error(errors);
-    }
-  
-    // do something great with this precious data
-    // console.log(data);
+export async function startFetchMyQuery() {
+  const { errors, data } = await fetchMyQuery();
+  if (errors) {
+    // handle those errors like a pro
+    console.error(errors);
   }
-  
+  // do something great with this precious data
+  console.log(data);
+}
+startFetchMyQuery();
